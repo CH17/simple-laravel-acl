2f58933b1b366fd7a1826df5483dd621ea7fdadd
Choyon B <contact.choyon@gmail.com>
2020-02-09 20:17:25 +0600

    Database setup, migration and seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        
+        $this->call(RolesTableSeeder::class);
+
+        $users = factory(App\User::class, 5)
+           ->create()
+           ->each(function ($user) {
+                $user->role()->attach(App\Role::inRandomOrder()->first());
+            });
     }
 }
